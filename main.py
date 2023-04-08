794ba5b92d74cb71d96438cc62f2e4ed77787fc9
Andray <robot-tnt@yandex.ru>
2023-04-09 02:14:42 +0300

    jurnal_csv

---
+++ main.py
@@ -0,0 +1,179 @@
+from datetime import datetime
+
+
+def Add():
+    id = None
+    f = open("index.csv", "r+", encoding="UTF-8")
+    while not id:
+        a = input('Введите id заметки: ')
+        if a.title() == "Выход":
+            break
+        if a.isdigit() and int(a) > 0:
+            no_repeat = True
+            for line in f:
+                index = line.find(';')
+                line = int(line[:index])
+                if int(a) == line:
+                    print("ID уже существует")
+                    no_repeat = False
+                    break
+            if no_repeat:
+                name = input("Введите заголовок заметки: ").upper()
+                text = input("Введите текст заметки: ")
+                f.write(f'{int(a)};{name};{text};{datetime.now()}\n')
+                f.close()
+                print('Запись добавлена')
+                id = int(a)
+        else:
+            print('ID должен состоять из одних цифр и быть больше 0')
+
+
+def Del():
+    id = None
+    while not id:
+        a = input('Введите id заметки: ')
+        if a.title() == "Выход":
+            break
+        if a.isdigit() and int(a) > 0:
+            with open("index.csv", "r", encoding="UTF-8") as f:
+                notes = f.readlines()
+            object_lost = True
+            for line in notes:
+                index = line.find(';')
+                index_int = int(line[:index])
+                if int(a) == index_int:
+                    notes.remove(line)
+                    with open("index.csv", "w", encoding="UTF-8") as f:
+                        f.writelines(notes)
+                    print('Заметка удалена')
+                    id = int(a)
+                    object_lost = False
+                    break
+            if object_lost:
+                print("Заметка отсутсвует")
+        else:
+            print('ID должен состоять из одних цифр и быть больше 0')
+
+
+def Edit():
+    id = None
+    while not id:
+        a = input('Введите id заметки: ')
+        if a.title() == "Выход":
+            break
+        if a.isdigit() and int(a) > 0:
+            with open("index.csv", "r", encoding="UTF-8") as f:
+                notes = f.readlines()
+            object_lost = True
+            exit_code = False
+            for i in range(len(notes)):
+                index = notes[i].find(';')
+                index_int = int(notes[i][:index])
+                if int(a) == index_int:
+                    edit_line = notes[i]
+                    while True:
+                        input_query = input('Хотите исправить заголовок или текст? ').title()
+                        t = ['Заголовок', 'Текст']
+                        if input_query in t:
+                            correct = input('Введите новый заголовок: ' if input_query == t[0] else 'Введите новый текст: ').title()
+                            second_index = edit_line.find(';', index + 1)
+                            third_index = edit_line.find(';', second_index + 1)
+                            index1 = index + 1 if input_query == t[0] else second_index + 1
+                            index2 = second_index if input_query == t[0] else third_index
+                            edit_line = edit_line.replace(edit_line[index1:index2], correct)
+                            last_index = edit_line.rfind(';')
+                            edit_line = edit_line.replace(edit_line[last_index + 1:], f"{datetime.now()}\n")
+                            notes[i] = edit_line
+                            break
+                        elif input_query == 'Выход':
+                            exit_code = True
+                            break
+                        else:
+                            print('Ошибка: Некорректный ввод')
+                    if exit_code:
+                        break
+                    with open("index.csv", "w", encoding="UTF-8") as f:
+                        f.writelines(notes)
+                    print('Заметка изменена')
+                    id = int(a)
+                    object_lost = False
+                    break
+            if exit_code:
+                break
+            if object_lost:
+                print("Заметка отсутсвует")
+        else:
+            print('ID должен состоять из одних цифр и быть больше 0')
+
+
+def Show():
+    with open("index.csv", "r", encoding="UTF-8") as f:
+        notes = f.readlines()
+    type = input("Фильтрация по дате или нет? ").title()
+    if type == "Да":
+        date = input("Введите дату в формате ГГГГ-ММ-ДД: ")
+        show_elem = 0
+        for line in notes:
+            last_index = line.rfind(';') + 1
+            if line[last_index:last_index + 10] == date:
+                show_elem += 1
+                print(line.strip())
+        if not show_elem:
+            print("Записей нет")
+    elif type == "Нет":
+        choise = input("Показать все записи? ").title()
+        if choise == 'Да':
+            for line in notes:
+                print(line.strip())
+        elif choise == 'Нет':
+            id_line = input("Введите id: ")
+            if id_line.isdigit() and int(id_line) > 0:
+                for line in notes:
+                    elem = int(line[:line.find(';')])
+                    if int(id_line) == elem:
+                        print(line.strip())
+                        break
+            else:
+                print("Записи нет")
+        elif choise == 'Выход':
+            return
+        else:
+            print('Ошибка: Некорректный ввод')
+    elif type == "Выход":
+        return
+    else:
+        print('Ошибка: Некорректный ввод')
+
+
+def Help():
+    print("""    Добавить - Добавить новую заметку
+    Удалить - Удалить существующую заметку
+    Редактировать - Редактировать существующую заметку
+    Показать - Показать список всех заметок
+    Выход - Выйти из приложения
+    Помощь - Доступные команды""")
+
+
+if __name__ == '__main__':
+    seans = True
+    while seans:
+
+        command = input('Выберите действие: ').title()
+
+        match command:
+            case "Добавить":
+                Add()
+            case "Удалить":
+                Del()
+            case "Редактировать":
+                Edit()
+            case "Показать":
+                Show()
+            case "Помощь":
+                Help()
+            case "Выход":
+                print("Благодарим за пользование нашей прораммой")
+                seans = False
+            case _:
+                print("Введена неверная команда")
+                Help()
